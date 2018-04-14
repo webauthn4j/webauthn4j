a85c11124dd2060ef74a6e44aedccd56b69f96e8
ynojima <mail@ynojima.net>
2018-04-14 16:14:34 +0900

    Add test cases

--- webauthn4j-core/src/test/java/com/webauthn4j/util/AssertUtilTest.java
+++ webauthn4j-core/src/test/java/com/webauthn4j/util/AssertUtilTest.java
@@ -1,6 +1,11 @@
 package com.webauthn4j.util;
 
+import org.assertj.core.util.Arrays;
 import org.junit.Test;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -17,5 +22,35 @@ public class AssertUtilTest {
         assertThatThrownBy(()->{
             AssertUtil.notNull(null, "message");
         }).isInstanceOf(IllegalArgumentException.class).hasMessage("message");
+    }
+
+    @Test
+    public void notEmpty_test_with_list(){
+        AssertUtil.notEmpty(Collections.singletonList(new Object()), "message");
+    }
+
+    @Test
+    public void notEmpty_test_with_array(){
+        AssertUtil.notEmpty(Arrays.array(new Object()), "message");
+    }
+
+    @Test
+    public void notEmpty_test_with_null_as_set(){
+        assertThatThrownBy(()-> AssertUtil.notEmpty((Set)null, "message")).isInstanceOf(IllegalArgumentException.class).hasMessage("message");
+    }
+
+    @Test
+    public void notEmpty_test_with_null_as_array(){
+        assertThatThrownBy(()-> AssertUtil.notEmpty((Object[]) null, "message")).isInstanceOf(IllegalArgumentException.class).hasMessage("message");
+    }
+
+    @Test
+    public void notEmpty_test_with_empty_set(){
+        assertThatThrownBy(()-> AssertUtil.notEmpty(new HashSet<>(), "message")).isInstanceOf(IllegalArgumentException.class).hasMessage("message");
+    }
+
+    @Test
+    public void notEmpty_test_with_empty_array(){
+        assertThatThrownBy(()-> AssertUtil.notEmpty(new Object[0], "message")).isInstanceOf(IllegalArgumentException.class).hasMessage("message");
     }
 }
