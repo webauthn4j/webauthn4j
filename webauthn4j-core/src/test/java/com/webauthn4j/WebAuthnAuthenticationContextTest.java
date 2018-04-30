b0e923e7a522da2de7cfe68e8be66f605ab9e23c
ynojima <mail@ynojima.net>
2018-05-01 08:21:44 +0900

    Add some test cases

---
+++ webauthn4j-core/src/test/java/com/webauthn4j/WebAuthnAuthenticationContextTest.java
@@ -0,0 +1,34 @@
+package com.webauthn4j;
+
+import com.webauthn4j.client.CollectedClientData;
+import com.webauthn4j.converter.AuthenticatorDataConverter;
+import com.webauthn4j.converter.CollectedClientDataConverter;
+import com.webauthn4j.test.TestUtil;
+import org.junit.Test;
+
+import static com.webauthn4j.client.CollectedClientData.TYPE_WEBAUTHN_GET;
+import static com.webauthn4j.test.TestUtil.createAuthenticatorData;
+import static com.webauthn4j.test.TestUtil.createClientData;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+public class WebAuthnAuthenticationContextTest {
+
+    @Test
+    public void getter_test(){
+
+        String credentialId = "";
+        byte[] collectedClientData = new CollectedClientDataConverter().convertToBytes(createClientData(TYPE_WEBAUTHN_GET));
+        byte[] authenticatorData = new AuthenticatorDataConverter().convertToBytes(createAuthenticatorData());
+        byte[] signature = new byte[]{0x01, 0x23};
+        RelyingParty relyingParty = mock(RelyingParty.class);
+        WebAuthnAuthenticationContext target = new WebAuthnAuthenticationContext(
+                credentialId, collectedClientData, authenticatorData, signature, relyingParty);
+        assertThat(target.getCredentialId()).isEqualTo(credentialId);
+        assertThat(target.getCollectedClientData()).isEqualTo(collectedClientData);
+        assertThat(target.getCollectedClientDataJson()).isEqualTo(new String(collectedClientData));
+        assertThat(target.getAuthenticatorData()).isEqualTo(authenticatorData);
+        assertThat(target.getSignature()).isEqualTo(signature);
+        assertThat(target.getRelyingParty()).isEqualTo(relyingParty);
+    }
+}
