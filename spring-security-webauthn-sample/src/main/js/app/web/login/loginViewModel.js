78fdb879c672881e55872329130c671a93e0e6bd
ynojima <mail@ynojima.net>
2018-04-30 12:40:46 +0900

    Remove passwod type CredentialManagement API support from sample
    to focus on WebAuthn API

--- spring-security-webauthn-sample/src/main/js/app/web/login/loginViewModel.js
+++ spring-security-webauthn-sample/src/main/js/app/web/login/loginViewModel.js
@@ -16,9 +16,6 @@ let LoginViewModel = function(){
             if(searchParams.has("error")) {
                 //nop
             }
-            else{
-                _this.tryLoginWithSavedPasswordCredential();
-            }
             _this.setupEventListeners();
         }
     });
@@ -104,56 +101,6 @@ LoginViewModel.prototype.loginByFormSubmission = function (username, password, c
     loginForm.find("input[name='collectedClientData']").val(base64url.encode(clientData));
     loginForm.find("input[name='authenticatorData']").val(base64url.encode(authenticatorData));
     loginForm.find("input[name='signature']").val(base64url.encode(signature));
-    loginForm.submit();
-};
-
-
-LoginViewModel.prototype.tryLoginWithSavedPasswordCredential = function (){
-    let _this = this;
-
-    return _this.getSavedPasswordCredential()
-        .then(function (credential) { //succeeded to retrieve credential
-            if(typeof credential === "undefined"){
-                return Promise.reject("No credential is chosen.");
-            }
-            else if (credential.type === "password") {
-                return _this.loginWithSavedPasswordCredential(credential);
-            }
-            else {
-                return Promise.reject("Unexpected credential type is returned.");
-            }
-        })
-        .catch(function (error) { // fall back to login form
-            console.error(error);
-        });
-};
-
-LoginViewModel.prototype.getSavedPasswordCredential = function (){
-    let _this = this;
-
-    if(typeof PasswordCredential === "undefined"){
-        return Promise.reject("Credential Management API is not supported.");
-    }
-
-    return navigator.credentials.get({
-        mediation: "optional",
-        password: true
-    });
-};
-
-LoginViewModel.prototype.loginWithSavedPasswordCredential = function (credential) {
-    let _this = this;
-
-    let username = credential.id;
-    let password = credential.password;
-
-    _this.loginByFormSubmission(username, password);
-};
-
-LoginViewModel.prototype.loginByFormSubmission = function (username, password) {
-    let loginForm = $("#login-form");
-    loginForm.find("input[name='username']").val(username);
-    loginForm.find("input[name='rawPassword']").val(password);
     loginForm.submit();
 };
 
