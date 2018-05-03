fc6d8d7cd156328a004e3a725ce0ceee7152396f
ynojima <mail@ynojima.net>
2018-05-03 18:53:53 +0900

    Polish code

--- spring-security-webauthn-core/src/test/java/net/sharplab/springframework/security/webauthn/context/provider/WebAuthnAuthenticationContextProviderImplTest.java
+++ spring-security-webauthn-core/src/test/java/net/sharplab/springframework/security/webauthn/context/provider/WebAuthnAuthenticationContextProviderImplTest.java
@@ -29,7 +29,7 @@ import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.util.Base64Utils;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
--- spring-security-webauthn-sample/src/test/java/net/sharplab/springframework/security/webauthn/sample/app/web/admin/UserControllerSpringTest.java
+++ spring-security-webauthn-sample/src/test/java/net/sharplab/springframework/security/webauthn/sample/app/web/admin/UserControllerSpringTest.java
@@ -30,7 +30,6 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.core.Is.is;
-import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
