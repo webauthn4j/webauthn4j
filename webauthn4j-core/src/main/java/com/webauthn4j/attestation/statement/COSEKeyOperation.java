235a8aec95d5d2c90b9d4b79de107ee7cd86bc44
ynojima <mail@ynojima.net>
2018-05-26 11:12:58 +0900

    Add COSEKeyOperation test cases

--- webauthn4j-core/src/main/java/com/webauthn4j/attestation/statement/COSEKeyOperation.java
+++ webauthn4j-core/src/main/java/com/webauthn4j/attestation/statement/COSEKeyOperation.java
@@ -1,5 +1,8 @@
 package com.webauthn4j.attestation.statement;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum  COSEKeyOperation {
     SIGN(1),
     VERIFY(2),
@@ -12,12 +15,41 @@ public enum  COSEKeyOperation {
     MAC_CREATE(9),
     MAC_VERIFY(10);
 
-    int value;
+    private int value;
 
     COSEKeyOperation(int value){
         this.value = value;
     }
 
+    @JsonCreator
+    public static COSEKeyOperation create(int value) {
+        switch (value) {
+            case 1:
+                return COSEKeyOperation.SIGN;
+            case 2:
+                return COSEKeyOperation.VERIFY;
+            case 3:
+                return COSEKeyOperation.ENCRYPT;
+            case 4:
+                return COSEKeyOperation.DECRYPT;
+            case 5:
+                return COSEKeyOperation.WRAP_KEY;
+            case 6:
+                return COSEKeyOperation.UNWRAP_KEY;
+            case 7:
+                return COSEKeyOperation.DERIVE_KEY;
+            case 8:
+                return COSEKeyOperation.DERIVE_BITS;
+            case 9:
+                return COSEKeyOperation.MAC_CREATE;
+            case 10:
+                return COSEKeyOperation.MAC_VERIFY;
+            default:
+                throw new IllegalArgumentException("value is out of range");
+        }
+    }
+
+    @JsonValue
     public int getValue() {
         return value;
     }
---
+++ webauthn4j-core/src/test/java/com/webauthn4j/attestation/statement/COSEKeyOperationTest.java
@@ -0,0 +1,33 @@
+package com.webauthn4j.attestation.statement;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class COSEKeyOperationTest {
+
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    @Test
+    public void create_test(){
+
+        assertThat(COSEKeyOperation.create(1)).isEqualTo(COSEKeyOperation.SIGN);
+        assertThat(COSEKeyOperation.create(2)).isEqualTo(COSEKeyOperation.VERIFY);
+        assertThat(COSEKeyOperation.create(3)).isEqualTo(COSEKeyOperation.ENCRYPT);
+        assertThat(COSEKeyOperation.create(4)).isEqualTo(COSEKeyOperation.DECRYPT);
+        assertThat(COSEKeyOperation.create(5)).isEqualTo(COSEKeyOperation.WRAP_KEY);
+        assertThat(COSEKeyOperation.create(6)).isEqualTo(COSEKeyOperation.UNWRAP_KEY);
+        assertThat(COSEKeyOperation.create(7)).isEqualTo(COSEKeyOperation.DERIVE_KEY);
+        assertThat(COSEKeyOperation.create(8)).isEqualTo(COSEKeyOperation.DERIVE_BITS);
+        assertThat(COSEKeyOperation.create(9)).isEqualTo(COSEKeyOperation.MAC_CREATE);
+        assertThat(COSEKeyOperation.create(10)).isEqualTo(COSEKeyOperation.MAC_VERIFY);
+
+        assertThatThrownBy(()-> COSEKeyOperation.create(0)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(()-> COSEKeyOperation.create(11)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void getValueTest(){
+        assertThat(COSEKeyOperation.SIGN.getValue()).isEqualTo(1);
+    }
+}
