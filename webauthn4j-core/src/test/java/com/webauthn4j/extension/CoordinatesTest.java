cd3b0994a82a90cea34c8c40975abc20cf36fe10
ynojima <mail@ynojima.net>
2018-05-30 00:50:37 +0900

    CoordinatesTest

---
+++ webauthn4j-core/src/test/java/com/webauthn4j/extension/CoordinatesTest.java
@@ -0,0 +1,38 @@
+package com.webauthn4j.extension;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CoordinatesTest {
+
+    @Test
+    public void getter_test(){
+        Coordinates instance = new Coordinates(
+            12.34,
+            23.45,
+            34.56,
+            4.5,
+            5.6,
+            7.8,
+             8.9
+        );
+
+        assertThat(instance.getLatitude()).isEqualTo(12.34);
+        assertThat(instance.getLongitude()).isEqualTo(23.45);
+        assertThat(instance.getAltitude()).isEqualTo(34.56);
+        assertThat(instance.getAccuracy()).isEqualTo(4.5);
+        assertThat(instance.getAltitudeAccuracy()).isEqualTo(5.6);
+        assertThat(instance.getHeading()).isEqualTo(7.8);
+        assertThat(instance.getSpeed()).isEqualTo(8.9);
+    }
+
+    @Test
+    public void equals_hashCode_test(){
+        Coordinates instanceA = new Coordinates();
+        Coordinates instanceB = new Coordinates();
+
+        assertThat(instanceA).isEqualTo(instanceB);
+        assertThat(instanceA).hasSameHashCodeAs(instanceB);
+    }
+}
