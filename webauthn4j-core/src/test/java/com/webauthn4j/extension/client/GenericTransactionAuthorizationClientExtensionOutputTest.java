c4d61ad01ac2e5b85aeede58fca5c3e7451bfd5c
ynojima <mail@ynojima.net>
2018-07-03 00:19:50 +0900

    Add some test cases

---
+++ webauthn4j-core/src/test/java/com/webauthn4j/extension/client/GenericTransactionAuthorizationClientExtensionOutputTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2002-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.webauthn4j.extension.client;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class GenericTransactionAuthorizationClientExtensionOutputTest {
+
+    @Test
+    public void test(){
+        GenericTransactionAuthorizationClientExtensionOutput target
+                = new GenericTransactionAuthorizationClientExtensionOutput(new GenericTransactionAuthorizationClientExtensionOutput.TxAuthnGenericArg());
+        assertThat(target.getIdentifier()).isEqualTo(GenericTransactionAuthorizationClientExtensionOutput.ID);
+    }
+
+    @Test
+    public void TxAuthnGenericArg_getter_setter_test() {
+        GenericTransactionAuthorizationClientExtensionOutput.TxAuthnGenericArg target
+                = new GenericTransactionAuthorizationClientExtensionOutput.TxAuthnGenericArg("type", new byte[32]);
+        assertThat(target.getContentType()).isEqualTo("type");
+        assertThat(target.getContent()).isEqualTo(new byte[32]);
+    }
+    @Test
+    public void TxAuthnGenericArg_equals_hashCode_test(){
+        GenericTransactionAuthorizationClientExtensionOutput.TxAuthnGenericArg instanceA = new GenericTransactionAuthorizationClientExtensionOutput.TxAuthnGenericArg();
+        GenericTransactionAuthorizationClientExtensionOutput.TxAuthnGenericArg instanceB = new GenericTransactionAuthorizationClientExtensionOutput.TxAuthnGenericArg();
+
+        assertThat(instanceA).isEqualTo(instanceB);
+        assertThat(instanceA).hasSameHashCodeAs(instanceB);
+    }
+}
