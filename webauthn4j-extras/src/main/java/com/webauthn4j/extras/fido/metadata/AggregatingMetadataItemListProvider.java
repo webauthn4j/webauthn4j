a8323b8f95af3f2720a89fe007ca1a902338a43a
ynojima <mail@ynojima.net>
2019-02-04 11:19:42 +0900

    Add AggregatingMetadataItemListProvider

---
+++ webauthn4j-extras/src/main/java/com/webauthn4j/extras/fido/metadata/AggregatingMetadataItemListProvider.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2002-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.webauthn4j.extras.fido.metadata;
+
+import com.webauthn4j.response.attestation.authenticator.AAGUID;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class AggregatingMetadataItemListProvider<T extends MetadataItem> implements MetadataItemListProvider<T>{
+
+    private List<MetadataItemListProvider<T>> metadataItemListProviders;
+
+    public AggregatingMetadataItemListProvider(List<MetadataItemListProvider<T>> metadataItemListProviders) {
+        this.metadataItemListProviders = metadataItemListProviders;
+    }
+
+    @Override
+    public Map<AAGUID, List<T>> provide() {
+        return metadataItemListProviders.stream()
+                .flatMap(provider -> provider.provide().entrySet().stream())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+}
