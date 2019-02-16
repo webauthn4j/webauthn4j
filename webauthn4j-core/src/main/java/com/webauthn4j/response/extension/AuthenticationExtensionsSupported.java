92ac837c28d5157b4b72a61372a415a087221ecf
ynojima <mail@ynojima.net>
2019-02-16 23:24:50 +0900

    Make AuthenticationExtensionsSupported immutable

--- webauthn4j-core/src/main/java/com/webauthn4j/response/extension/AuthenticationExtensionsSupported.java
+++ webauthn4j-core/src/main/java/com/webauthn4j/response/extension/AuthenticationExtensionsSupported.java
@@ -16,15 +16,52 @@
 
 package com.webauthn4j.response.extension;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.webauthn4j.util.AssertUtil;
+
 import java.io.Serializable;
-import java.util.ArrayList;
+import java.util.AbstractList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
-public class AuthenticationExtensionsSupported extends ArrayList<String> implements Serializable {
+public class AuthenticationExtensionsSupported extends AbstractList<String> implements Serializable {
 
-    public AuthenticationExtensionsSupported(List<String> values){
-        super();
-        this.addAll(values);
+    private String[] extensions;
+    private final int size;
+
+    @JsonCreator
+    public AuthenticationExtensionsSupported(List<String> extensions){
+        AssertUtil.notNull(extensions, "extensions must not be null");
+        this.size = extensions.size();
+        this.extensions = extensions.toArray(new String[this.size]);
     }
 
+    @Override
+    public String get(int index) {
+        return extensions[index];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        AuthenticationExtensionsSupported strings = (AuthenticationExtensionsSupported) o;
+        return size == strings.size &&
+                Arrays.equals(extensions, strings.extensions);
+    }
+
+    @Override
+    public int hashCode() {
+
+        int result = Objects.hash(super.hashCode(), size);
+        result = 31 * result + Arrays.hashCode(extensions);
+        return result;
+    }
 }
