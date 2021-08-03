beed0ea363e167175345bdb92093627ad1c7cad7
davor <davor.davidovikj@adyen.com>
2021-08-03 10:46:33 +0200

    add static import

--- webauthn4j-core/src/test/java/com/webauthn4j/validator/PaymentOriginValidatorImplTest.java
+++ webauthn4j-core/src/test/java/com/webauthn4j/validator/PaymentOriginValidatorImplTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class PaymentOriginValidatorImplTest {
