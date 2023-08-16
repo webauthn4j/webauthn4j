1465f21a1442568f2ef9fcdeff7b58e2da4300c0
Yoshikazu Nojima <mail@ynojima.net>
2023-08-16 22:37:10 +0900

    Add contextual properties to NotAllowAlgorithmException and NotAllowedCredentialException

--- webauthn4j-core/src/main/java/com/webauthn4j/verifier/exception/NotAllowedCredentialIdException.java
+++ webauthn4j-core/src/main/java/com/webauthn4j/verifier/exception/NotAllowedCredentialIdException.java
@@ -18,17 +18,50 @@ package com.webauthn4j.verifier.exception;
 
 import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
+
 public class NotAllowedCredentialIdException extends VerificationException {
+
+    private final List<byte[]> expected;
+    private final byte[] actual;
+
+    public NotAllowedCredentialIdException(@Nullable String message, @Nullable List<byte[]> expected, @Nullable byte[] actual, @Nullable Throwable cause) {
+        super(message, cause);
+        this.expected = expected;
+        this.actual = actual;
+    }
+
+    public NotAllowedCredentialIdException(@Nullable String message, @Nullable List<byte[]> expected, @Nullable byte[] actual) {
+        super(message);
+        this.expected = expected;
+        this.actual = actual;
+    }
 
     public NotAllowedCredentialIdException(@Nullable String message, @Nullable Throwable cause) {
         super(message, cause);
+        this.expected = null;
+        this.actual = null;
     }
 
     public NotAllowedCredentialIdException(@Nullable String message) {
         super(message);
+        this.expected = null;
+        this.actual = null;
     }
 
     public NotAllowedCredentialIdException(@Nullable Throwable cause) {
         super(cause);
+        this.expected = null;
+        this.actual = null;
+    }
+
+    @Nullable
+    public List<byte[]> getExpected() {
+        return expected;
+    }
+
+    @Nullable
+    public byte[] getActual() {
+        return actual;
     }
 }
