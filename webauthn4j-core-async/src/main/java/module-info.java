15279b1051059e09b15b1b5398c5eaa8bc20d1f9
Yoshikazu Nojima <mail@ynojima.net>
2024-11-14 23:56:29 +0900

    Correct webauthn4j-core-async module-info
    As some packages are not included by mistake

--- webauthn4j-core-async/src/main/java/module-info.java
+++ webauthn4j-core-async/src/main/java/module-info.java
@@ -9,5 +9,15 @@ module com.webauthn4j.core.async {
     exports com.webauthn4j.async;
     exports com.webauthn4j.async.anchor;
     exports com.webauthn4j.async.verifier;
+    exports com.webauthn4j.async.verifier.attestation.statement;
+    exports com.webauthn4j.async.verifier.attestation.statement.androidkey;
+    exports com.webauthn4j.async.verifier.attestation.statement.androidsafetynet;
+    exports com.webauthn4j.async.verifier.attestation.statement.apple;
+    exports com.webauthn4j.async.verifier.attestation.statement.none;
+    exports com.webauthn4j.async.verifier.attestation.statement.packed;
+    exports com.webauthn4j.async.verifier.attestation.statement.tpm;
+    exports com.webauthn4j.async.verifier.attestation.statement.u2f;
+    exports com.webauthn4j.async.verifier.attestation.trustworthiness.certpath;
+    exports com.webauthn4j.async.verifier.attestation.trustworthiness.self;
     exports com.webauthn4j.async.util.internal to com.webauthn4j.metadata.async;
 }
