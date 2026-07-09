7c73683460e61f2c2b066d8e80173fab9d26c297
Yoshikazu Nojima <mail@ynojima.net>
2026-07-09 15:23:56 +0900

    Reject non-integer values in COSEAlgorithmIdentifierDeserializer
    Both JSON and CBOR deserializers previously accepted non-integer
    values for COSEAlgorithmIdentifier (e.g., string "-7" coerced to
    integer -7). This violates the WebAuthn and CTAP specs which require
    alg to be an integer type. Now both deserializers check the token
    type and reject non-integer values with InvalidFormatException.

--- webauthn4j-core/src/main/java/com/webauthn4j/converter/jackson/deserializer/cbor/COSEAlgorithmIdentifierDeserializer.java
+++ webauthn4j-core/src/main/java/com/webauthn4j/converter/jackson/deserializer/cbor/COSEAlgorithmIdentifierDeserializer.java
@@ -3,8 +3,10 @@ package com.webauthn4j.converter.jackson.deserializer.cbor;
 import com.webauthn4j.data.attestation.statement.COSEAlgorithmIdentifier;
 import org.jetbrains.annotations.NotNull;
 import tools.jackson.core.JsonParser;
+import tools.jackson.core.JsonToken;
 import tools.jackson.databind.DeserializationContext;
 import tools.jackson.databind.deser.std.StdDeserializer;
+import tools.jackson.databind.exc.InvalidFormatException;
 
 public class COSEAlgorithmIdentifierDeserializer extends StdDeserializer<COSEAlgorithmIdentifier> {
 
@@ -14,6 +16,9 @@ public class COSEAlgorithmIdentifierDeserializer extends StdDeserializer<COSEAlg
 
     @Override
     public COSEAlgorithmIdentifier deserialize(@NotNull JsonParser p, @NotNull DeserializationContext ctxt) {
+        if (p.currentToken() != JsonToken.VALUE_NUMBER_INT) {
+            throw InvalidFormatException.from(p, "Expected an integer value for COSEAlgorithmIdentifier", p.getText(), COSEAlgorithmIdentifier.class);
+        }
         long value = p.getValueAsLong();
         return COSEAlgorithmIdentifier.create(value);
     }
