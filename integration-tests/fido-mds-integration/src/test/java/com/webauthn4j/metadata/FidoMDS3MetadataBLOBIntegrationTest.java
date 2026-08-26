20162518f43291cf9a7d34cca2e8fbe2fc4d0ffb
Yoshikazu Nojima <mail@ynojima.net>
2026-08-26 22:57:06 +0900

    Fix FIDO MDS integration test for Root R46

--- integration-tests/fido-mds-integration/src/test/java/com/webauthn4j/metadata/FidoMDS3MetadataBLOBIntegrationTest.java
+++ integration-tests/fido-mds-integration/src/test/java/com/webauthn4j/metadata/FidoMDS3MetadataBLOBIntegrationTest.java
@@ -7,7 +7,6 @@ import com.webauthn4j.converter.util.ObjectConverter;
 import com.webauthn4j.metadata.converter.jackson.WebAuthnMetadataJSONModule;
 import com.webauthn4j.metadata.data.MetadataBLOB;
 import com.webauthn4j.metadata.data.MetadataBLOBPayload;
-import com.webauthn4j.util.Base64Util;
 import com.webauthn4j.util.CertificateUtil;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -34,26 +33,8 @@ import static org.mockito.Mockito.when;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class FidoMDS3MetadataBLOBIntegrationTest {
 
-    private static final X509Certificate ROOT_CERTIFICATE = CertificateUtil.generateX509Certificate(Base64Util.decode(
-            "MIIDXzCCAkegAwIBAgILBAAAAAABIVhTCKIwDQYJKoZIhvcNAQELBQAwTDEgMB4G" +
-                    "A1UECxMXR2xvYmFsU2lnbiBSb290IENBIC0gUjMxEzARBgNVBAoTCkdsb2JhbFNp" +
-                    "Z24xEzARBgNVBAMTCkdsb2JhbFNpZ24wHhcNMDkwMzE4MTAwMDAwWhcNMjkwMzE4" +
-                    "MTAwMDAwWjBMMSAwHgYDVQQLExdHbG9iYWxTaWduIFJvb3QgQ0EgLSBSMzETMBEG" +
-                    "A1UEChMKR2xvYmFsU2lnbjETMBEGA1UEAxMKR2xvYmFsU2lnbjCCASIwDQYJKoZI" +
-                    "hvcNAQEBBQADggEPADCCAQoCggEBAMwldpB5BngiFvXAg7aEyiie/QV2EcWtiHL8" +
-                    "RgJDx7KKnQRfJMsuS+FggkbhUqsMgUdwbN1k0ev1LKMPgj0MK66X17YUhhB5uzsT" +
-                    "gHeMCOFJ0mpiLx9e+pZo34knlTifBtc+ycsmWQ1z3rDI6SYOgxXG71uL0gRgykmm" +
-                    "KPZpO/bLyCiR5Z2KYVc3rHQU3HTgOu5yLy6c+9C7v/U9AOEGM+iCK65TpjoWc4zd" +
-                    "QQ4gOsC0p6Hpsk+QLjJg6VfLuQSSaGjlOCZgdbKfd/+RFO+uIEn8rUAVSNECMWEZ" +
-                    "XriX7613t2Saer9fwRPvm2L7DWzgVGkWqQPabumDk3F2xmmFghcCAwEAAaNCMEAw" +
-                    "DgYDVR0PAQH/BAQDAgEGMA8GA1UdEwEB/wQFMAMBAf8wHQYDVR0OBBYEFI/wS3+o" +
-                    "LkUkrk1Q+mOai97i3Ru8MA0GCSqGSIb3DQEBCwUAA4IBAQBLQNvAUKr+yAzv95ZU" +
-                    "RUm7lgAJQayzE4aGKAczymvmdLm6AC2upArT9fHxD4q/c2dKg8dEe3jgr25sbwMp" +
-                    "jjM5RcOO5LlXbKr8EpbsU8Yt5CRsuZRj+9xTaGdWPoO4zzUhw8lo/s7awlOqzJCK" +
-                    "6fBdRoyV3XpYKBovHd7NADdBj+1EbddTKJd+82cEHhXXipa0095MJ6RMG3NzdvQX" +
-                    "mcIfeg7jLQitChws/zyrVQ4PkX4268NXSb7hLi18YIvDQVETI53O9zJrlAGomecs" +
-                    "Mx86OyXShkDOOyyGeMlhLxS67ttVb9+E7gUJTb0o2HLO02JQZR7rkpeDMdmztcpH" +
-                    "WD9f"));
+    private static final X509Certificate ROOT_CERTIFICATE = CertificateUtil.generateX509Certificate(
+            FidoMDS3MetadataBLOBIntegrationTest.class.getResourceAsStream("/integration/component/root-r46.crt"));
 
     private byte[] blobBytes;
 
@@ -101,23 +82,23 @@ class FidoMDS3MetadataBLOBIntegrationTest {
 
     @Test
     void async_test() throws ExecutionException, InterruptedException {
-        Path rootCertificatePath = new File("src/test/resources/integration/component/root-r3.crt").toPath();
+        Path rootCertificatePath = new File("src/test/resources/integration/component/root-r46.crt").toPath();
         byte[] rootCertificateBytes = FileAsyncUtil.load(rootCertificatePath).toCompletableFuture().get();
         X509Certificate rootCertificate = CertificateUtil.generateX509Certificate(rootCertificateBytes);
 
-        Path gsextendvalsha2g3r3CrlPath = new File("src/test/resources/integration/component/gsextendvalsha2g3r3.crl").toPath();
-        byte[] crlBytes = FileAsyncUtil.load(gsextendvalsha2g3r3CrlPath).toCompletableFuture().get();
+        Path gsgccr46evtlsca2025CrlPath = new File("src/test/resources/integration/component/gsgccr46evtlsca2025.crl").toPath();
+        byte[] crlBytes = FileAsyncUtil.load(gsgccr46evtlsca2025CrlPath).toCompletableFuture().get();
 
-        Path rootR3CrlPath = new File("src/test/resources/integration/component/root-r3.crl").toPath();
-        byte[] rootR3CrlBytes = FileAsyncUtil.load(rootR3CrlPath).toCompletableFuture().get();
+        Path rootR46CrlPath = new File("src/test/resources/integration/component/root-r46.crl").toPath();
+        byte[] rootR46CrlBytes = FileAsyncUtil.load(rootR46CrlPath).toCompletableFuture().get();
 
         HttpAsyncClient httpAsyncClient = mock(HttpAsyncClient.class);
         when(httpAsyncClient.fetch(FidoMDS3MetadataBLOBAsyncProvider.DEFAULT_BLOB_ENDPOINT))
                 .thenReturn(CompletableFuture.completedFuture(new HttpClient.Response(200, new ByteArrayInputStream(blobBytes))));
-        when(httpAsyncClient.fetch("http://crl.globalsign.com/gs/gsextendvalsha2g3r3.crl"))
+        when(httpAsyncClient.fetch("http://crl.globalsign.com/gsgccr46evtlsca2025.crl"))
                 .thenReturn(CompletableFuture.completedFuture(new HttpClient.Response(200, new ByteArrayInputStream(crlBytes))));
-        when(httpAsyncClient.fetch("http://crl.globalsign.com/root-r3.crl"))
-                .thenReturn(CompletableFuture.completedFuture(new HttpClient.Response(200, new ByteArrayInputStream(rootR3CrlBytes))));
+        when(httpAsyncClient.fetch("http://crl.globalsign.com/rootr46.crl"))
+                .thenReturn(CompletableFuture.completedFuture(new HttpClient.Response(200, new ByteArrayInputStream(rootR46CrlBytes))));
 
         FidoMDS3MetadataBLOBAsyncProvider target = new FidoMDS3MetadataBLOBAsyncProvider(
                 new ObjectConverter(),
